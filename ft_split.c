143d8c06e1fd549d73fbc87dab255ee36d8df9fb
Zouhir El baz <zel-baz@c2r9p6.1337.ma>
2024-01-18 00:03:59 +0100

    ft_split() edit

--- ft_split.c
+++ ft_split.c
@@ -6,13 +6,13 @@
 /*   By: zel-baz <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 21:25:50 by zel-baz           #+#    #+#             */
-/*   Updated: 2024/01/11 21:25:52 by zel-baz          ###   ########.fr       */
+/*   Updated: 2024/01/18 00:03:25 by zel-baz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char **ft_split(char *str, char c)
+char **ft_split(char const *str, char c)
 {
 
 }
